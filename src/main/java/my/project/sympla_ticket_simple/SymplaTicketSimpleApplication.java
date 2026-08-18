989a196447e068cd2b8d76794e0d004e923a9f9c
Gustavo Henrique <gustavohenriqueferreirasantosf@gmail.com>
2026-08-18 18:09:09 -0300

    Setup inicial: estrutura do projeto, Docker Compose, Flyway, Secutiry, Swagger, PostgreSQL criado

---
+++ src/main/java/my/project/sympla_ticket_simple/SymplaTicketSimpleApplication.java
@@ -0,0 +1,13 @@
+package my.project.sympla_ticket_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymplaTicketSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymplaTicketSimpleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/project/sympla_ticket_simple/SymplaTicketSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package my.project.sympla_ticket_simple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymplaTicketSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
