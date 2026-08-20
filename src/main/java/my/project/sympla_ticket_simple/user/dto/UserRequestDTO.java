fd6bdbc7797e42e63c53051d7d566e636c3b3b34
Gustavo Henrique <gustavohenriqueferreirasantosf@gmail.com>
2026-08-20 11:31:37 -0300

    Implementando o Repository / DTOs

---
+++ src/main/java/my/project/sympla_ticket_simple/user/dto/UserRequestDTO.java
@@ -0,0 +1,12 @@
+package my.project.sympla_ticket_simple.user.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserResquestDTO(
+        @NotBlank @Size(min=15, max=100)String username,
+        @NotBlank @Email @Size(min=15, max=150) String email,
+        @NotBlank @Size(min=8, max=50) String password
+        ) {
+}
--- src/main/java/my/project/sympla_ticket_simple/user/dto/UserResquestDTO.java
+++
@@ -1,4 +0,0 @@
-package my.project.sympla_ticket_simple.user.dto;
-
-public class UserResquestDTO {
-}
