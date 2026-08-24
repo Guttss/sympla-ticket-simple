f24f706ec08138af465201db6904817ce100917d
Gustavo Henrique <gustavohenriqueferreirasantosf@gmail.com>
2026-08-24 09:53:05 -0300

    Implementando o metodo POST para criação de um novo usuario

--- src/main/java/my/project/sympla_ticket_simple/user/UserController.java
+++ src/main/java/my/project/sympla_ticket_simple/user/UserController.java
@@ -1,4 +1,28 @@
 package my.project.sympla_ticket_simple.user;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import my.project.sympla_ticket_simple.user.dto.UserRequestDTO;
+import my.project.sympla_ticket_simple.user.dto.UserResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(@Valid @RequestBody UserRequestDTO userRequestDTO){
+        UserResponseDTO userResponseDTO = userService.createUser(userRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDTO);
+    }
 }
