5795bbe61ecaeebe61a437b01ae863bdd2d21cf2
Gustavo Henrique <gustavohenriqueferreirasantosf@gmail.com>
2026-08-24 11:55:54 -0300

    Deixando o End point para criar usuario publico

--- src/main/java/my/project/sympla_ticket_simple/config/SecurityConfig.java
+++ src/main/java/my/project/sympla_ticket_simple/config/SecurityConfig.java
@@ -2,14 +2,30 @@ package my.project.sympla_ticket_simple.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(csrf -> csrf.disable());
+
+        http.authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, "/users").permitAll()
+                .requestMatchers("/error").permitAll().anyRequest().authenticated());
+
+        return http.build();
     }
 }
