147c81395e482a3c7a0583d92c2d0860d2828e90
Gustavo Henrique <gustavohenriqueferreirasantosf@gmail.com>
2026-08-27 11:36:46 -0300

    fix: Correção do arquivo .properties

---
+++ src/main/resources/db/migration/V1__create_tb_users_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tb_users(
+    id BIGSERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP
+);
