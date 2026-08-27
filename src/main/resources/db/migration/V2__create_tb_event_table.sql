abd2e7c30e94643b4f9ab19350d68f7f7ef7a7fd
Gustavo Henrique <gustavohenriqueferreirasantosf@gmail.com>
2026-08-27 12:42:21 -0300

    feat: Criando a tabela de eventos vinculado com um organizador/usuario

---
+++ src/main/resources/db/migration/V2__create_tb_event_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tb_events(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT NOT NULL,
+    start_date TIMESTAMP NOT NULL,
+    end_date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    location VARCHAR(200) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    user_id BIGINT NOT NULL,
+
+
+    CONSTRAINT fk_events_users FOREIGN KEY (user_id) REFERENCES tb_users(id)
+);
